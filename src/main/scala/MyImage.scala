53e1b51ed0293ea7682c262e14460e76eb94e655
Piotr Swędrak <piotrekswedrak@gmail.com>
2018-06-16 22:51:56 +0200

    refactoring

---
+++ src/main/scala/MyImage.scala
@@ -0,0 +1,81 @@
+import com.github.dunnololda.scage.ScageLib._
+import com.sksamuel.scrimage._
+import com.sksamuel.scrimage.filter._
+import com.sksamuel.scrimage.X11Colorlist._
+import java.io.File._
+import java.io._
+import javax.imageio.ImageIO
+
+class MyImage(val name: String){
+
+    val path = "resources/images/" + name
+
+    val image = ImageIO.read(new File(path))
+    var width = image.getWidth
+    var height = image.getHeight
+
+    var scageImage = com.github.dunnololda.scage.ScageLib.image(name, width, height, 0, 0, width, height)
+    var scrimageImage = Image(new java.io.File(path))
+
+    val resultName = name + "_result"
+    val resultPath = "resources/images/" + resultName
+
+    var resultFile = new File(resultPath)
+    resultFile.createNewFile()
+    scrimageImage.output(resultFile)
+
+    var scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    var scrimageImageResult = Image(new java.io.File(resultPath))
+
+    def flipX = {
+        resultFile = new java.io.File(resultPath)
+        scrimageImageResult = Image(resultFile)
+        scrimageImageResult.flipX.output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }
+
+    def flipY = {
+        resultFile = new java.io.File(resultPath)
+        scrimageImageResult = Image(resultFile)
+        scrimageImageResult.flipY.output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }
+
+    def frame = {
+        resultFile = new java.io.File(resultPath)
+        scrimageImageResult = Image(resultFile)
+        scrimageImageResult.scaleTo(width - 20, height - 20).pad(10, Color.Black).output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }
+
+    def blur = {
+        resultFile = new java.io.File(resultPath)
+        scrimageImageResult = Image(resultFile)
+        scrimageImageResult.filter(filter.BlurFilter).output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }
+
+    def bright = {
+        resultFile = new java.io.File(resultPath)
+        scrimageImageResult = Image(resultFile)
+        scrimageImageResult.filter(BrightnessFilter(5.0)).output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }
+
+     def chrome = {
+        resultFile = new java.io.File(resultPath)
+        scrimageImageResult = Image(resultFile)
+        scrimageImageResult.filter(filter.ChromeFilter(1.0f, 1.0f)).output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }     
+
+     def reload = {
+        scrimageImage.output(resultFile)
+        scageImageResult = com.github.dunnololda.scage.ScageLib.image(resultName, width, height, 0, 0, width, height)
+    }    
+
+    
+}
+
+
+        
--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -4,87 +4,45 @@ import com.sksamuel.scrimage.filter._
 import com.sksamuel.scrimage.X11Colorlist._
 import java.io.File._
 import java.io._
-
 import javax.imageio.ImageIO
+
 
 object Main extends ScageScreenApp("Scage App", 1000, 640){
 
+    backgroundColor = WHITE
+    var myImage = new MyImage("dice.png")
 
 
-
-  backgroundColor = WHITE
-
-   val photo1 = ImageIO.read(new File("resources/images/" + "dice.png"))
-
-   var width = photo1.getWidth
-   var height = photo1.getHeight
+    key(KEY_X, onKeyDown = {
+      myImage.flipX
+    })
 
 
-  var diceFile = new java.io.File("resources/images/" + "dice.png")
-  var diceImage = Image(diceFile)
+    key(KEY_Y, onKeyDown = {
+      myImage.flipX
+    })
 
-  var resultFile = new java.io.File("resources/images/result.png")
-  diceImage.output(resultFile)
+    key(KEY_F, onKeyDown = {
+      myImage.frame
+    })
 
-  var inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-  var imageInProcess = Image(resultFile)
+    key(KEY_B, onKeyDown = {
+      myImage.blur
+    })
 
-    var name = "dice2.png"
-  backgroundColor = WHITE
+    key(KEY_M, onKeyDown = {
+      myImage.bright
+    })
 
-    //key(KEY_RIGHT)
+    key(KEY_C, onKeyDown = {
+      myImage.chrome
+    })
 
-  key(KEY_X, onKeyDown = {
-    resultFile = new java.io.File("resources/images/result.png")
-    imageInProcess = Image(resultFile)
-    imageInProcess.flipX.output(resultFile)
-    inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-  })
+    key(KEY_R, onKeyDown = {
+      myImage.reload
+    })
 
-  key(KEY_Y, onKeyDown = {
-    resultFile = new java.io.File("resources/images/result.png")
-    imageInProcess = Image(resultFile)
-    imageInProcess.flipY.output(resultFile)
-    inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-  })
-
-  key(KEY_R, onKeyDown = {
-    resultFile = new java.io.File("resources/images/dice.png")
-    imageInProcess = Image(resultFile)
-    imageInProcess.output(new java.io.File("resources/images/result.png"))
-    inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-  })
-
-  key(KEY_B, onKeyDown = {
-     resultFile = new java.io.File("resources/images/result.png")
-     imageInProcess = Image(resultFile)
-     imageInProcess.filter(filter.BlurFilter).output(resultFile)
-   inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-})
-
-  key(KEY_O, onKeyDown = {
-     resultFile = new java.io.File("resources/images/result.png")
-     imageInProcess = Image(resultFile)
-     imageInProcess.filter(filter.ChromeFilter(1.0f, 1.0f)).output(resultFile)
-   inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-})
-
-  key(KEY_M, onKeyDown = {
-     resultFile = new java.io.File("resources/images/result.png")
-     imageInProcess = Image(resultFile)
-     imageInProcess.filter(BrightnessFilter(5.0)).output(resultFile)
-   inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-})
-
-  key(KEY_F, onKeyDown = {
-     resultFile = new java.io.File("resources/images/result.png")
-     imageInProcess = Image(resultFile)
-     imageInProcess.scaleTo(width - 20, height - 20).pad(10, Color.Black).output(resultFile)
- 
-    inProcessShow = com.github.dunnololda.scage.ScageLib.image("result.png", width, height, 0, 0, width, height)
-})
-
-     interface{
+    interface{
         print("options", Vec(770,450), RED)
         print("X - flipX", Vec(680,430), RED)
         print("Y - flipY", Vec(680,410), RED)
@@ -92,16 +50,17 @@ object Main extends ScageScreenApp("Scage App", 1000, 640){
         print("M - bright filter", Vec(680,370), RED)
         print("R - reload", Vec(680,350), RED)
         print("F - frame", Vec(680,330), RED)
-    }
+        print("C - chrome", Vec(680,310), RED)
+     }
 
 
     action(1){
 
-}
+    }
 
-render{
-    drawDisplayList(inProcessShow, Vec(250,300))
-}
+    render{
+        drawDisplayList(myImage.scageImageResult, Vec(250,300))
+    }
 
 
 }
